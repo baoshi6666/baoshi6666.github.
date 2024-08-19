c9e2e9ef007f58d9f817c21c7c2c84af6b57ce9c
wrwkej2 <1783838559@qq.com>
2024-08-19 15:21:48 +0800

    Site updated: 2024-08-19 15:21:48

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/03/hello-world/","2024/08/19/Newbing/"];function toRandomPost(){
+var posts=["2024/08/19/Newbing/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
