1aa76202b42f5b0d3ddd871c45857a0846d883ea
wrwkej2 <1783838559@qq.com>
2024-08-25 22:36:46 +0800

    Site updated: 2024-08-25 22:36:45

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/19/Newbing/"];function toRandomPost(){
+var posts=["2024/08/25/Newbing国内无需魔法，无需验证的部署方法/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
