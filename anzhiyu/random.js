1e87bbd14c2d47de32ee91e91f75fc8507e3297d
wrwkej2 <1783838559@qq.com>
2024-08-31 22:46:08 +0800

    Site updated: 2024-08-31 22:46:08

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/25/Newbing国内无需魔法，无需验证的部署方法/"];function toRandomPost(){
+var posts=["2024/08/25/Newbing国内无需魔法，无需验证的部署方法/","2024/08/31/黑毛毛虫不能吃豇豆/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
